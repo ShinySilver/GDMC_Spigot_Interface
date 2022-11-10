362ef935996d36825e948bfd783a1262d9bbf594
Silverly <36570018+ShinySilver@users.noreply.github.com>
2022-11-10 19:42:43 +0100

    Update 2_fetch_latest_interface_build.sh

--- GDMC Server/2_fetch_latest_interface_build.sh
+++ GDMC Server/2_fetch_latest_interface_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "This script is not done yet! The readme lied, you have to build the GDMC HTTP Interface Plugin yourself and place it under <GDMC Server/plugins/the_name_you_want.jar>"
+echo "This script is not done yet! The readme lied, you have to build the GDMC HTTP Interface Plugin yourself and place it under <GDMC Server/plugins/the_name_you_want.jar>. Moreover, you should download on spigotmc.org FastAsyncWorldEdit and place it in the same 'plugins' folder. I personnaly used the 2.4.2 snapshot version."
 
 echo ""
-echo "This script should:tm: clear the plugin folder of similarly named plugins, connect to git, find & download the latest released jar for a given version depending on the spigot version built by the user (ex. spigot-1.16.5.jar = 1.16.5). But since until now this project was not on git, a script cannot be made, right?"
+echo "This script should:tm: clear the plugin folder of similarly named plugins, connect to git, find & download the latest released jar for a given version depending on the spigot version built by the user (ex. spigot-1.16.5.jar = 1.16.5). Maybe later?"
 
 echo ""
 read -p "Press Enter to exit" ex
